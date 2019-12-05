76645bd307e416e6ce70e2a394c9d73476a7caac
rinellam <michael@michaelrinella.com>
2019-12-05 10:06:10 -0600

    update image folder

---
+++ js/ranger.js
@@ -0,0 +1,11 @@
+
+// play and pause button 
+function change( el )
+        {
+            if ( el.value === "Play" )
+                el.value = "Pause";
+            else
+                el.value = "Play";
+        }
+
+
